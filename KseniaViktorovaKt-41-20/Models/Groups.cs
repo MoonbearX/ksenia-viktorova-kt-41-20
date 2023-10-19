300a152944fcfd86a3cb67553c2432b6381a70eb
MoonbearX <viktorovamypp@gmail.com>
2023-10-19 12:42:11 +0300

    Changed models and context. Added configurations for SrudentsRecords.

--- KseniaViktorovaKt-41-20/Models/Groups.cs
+++ KseniaViktorovaKt-41-20/Models/Groups.cs
@@ -4,9 +4,5 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public ICollection<Students>? Student { get; set; }
-        public ICollection<Subjects>? Subject { get; set; }
-        public ICollection<RecordsSession>? RecordsSession { get; set; }
-        public ICollection<RecordsSemester>? RecordsSemester { get; set; }
     }
 }
